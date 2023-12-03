25044aa26c5492158c0c4636ac0de52ed18b9d9c
Siddhanta Acharya <siddhu12980@gmail.com>
2023-12-03 12:08:31 +0530

    adding home screen

--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class mobilescreenlayout extends StatefulWidget {
@@ -9,9 +11,24 @@ class mobilescreenlayout extends StatefulWidget {
 
 // ignore: camel_case_types
 class _mobilescreenlayoutState extends State<mobilescreenlayout> {
+  String username = "";
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getusername();
+  }
+
+  void getusername() async {
+    FirebaseFirestore.instance
+        .collection('users')
+        .doc(FirebaseAuth.instance.currentUser!.uid);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
         child: Text("this is mobile"),
       ),
