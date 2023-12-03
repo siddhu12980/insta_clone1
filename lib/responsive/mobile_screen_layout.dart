b4c2fff6d5cf5b1c144b49e1a0ca3f796b963646
Siddhanta Acharya <siddhu12980@gmail.com>
2023-12-03 16:16:09 +0530

    home screen

--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
+// ignore: camel_case_types
 class mobilescreenlayout extends StatefulWidget {
   const mobilescreenlayout({super.key});
 
@@ -21,16 +22,23 @@ class _mobilescreenlayoutState extends State<mobilescreenlayout> {
   }
 
   void getusername() async {
-    FirebaseFirestore.instance
+    DocumentSnapshot snap = await FirebaseFirestore.instance
         .collection('users')
-        .doc(FirebaseAuth.instance.currentUser!.uid);
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .get();
+
+    print(snap.data());
+
+    setState(() {
+      username = (snap.data() as Map<String, dynamic>)['username'];
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("this is mobile"),
+        child: Text(" user:$username"),
       ),
     );
   }
