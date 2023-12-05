c0f3f96332e06079fb8e232eb364b72846087bde
Siddhanta Acharya <siddhu12980@gmail.com>
2023-12-05 23:02:13 +0530

    continue

--- lib/responsive/web_screen_layout.dart
+++ lib/responsive/web_screen_layout.dart
@@ -6,7 +6,7 @@ class webscreenlayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Center(child: Text("this is web")),
     );
   }
