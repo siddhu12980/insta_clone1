8f5819a07d8b15bb9f21b8d71113a4d85d608307
Siddhanta Acharya <siddhu12980@gmail.com>
2023-12-10 22:38:11 +0530

    managing UI

--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -1,7 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:insta/providers/user_providers.dart';
+import 'package:insta/utils/color.dart';
 import 'package:provider/provider.dart';
 import 'package:insta/model/user.dart' as model;
 
@@ -24,18 +26,56 @@ class _mobilescreenlayoutState extends State<mobilescreenlayout> {
   @override
   Widget build(BuildContext context) {
     model.User user = Provider.of<UserProvider>(context).getUser;
-
+    int _page = 0;
     return Scaffold(
-        body: Center(
-          child: Text("Mobile user:${user.username}"),
-        ),
-        bottomNavigationBar: TabBar(
-          tabs: [
-            IconButton(
-              onPressed: () => greet(),
-              icon: const Icon(Icons.refresh),
-            )
-          ],
-        ));
+      body: Center(
+        child: Text("Mobile user:${user.username}"),
+      ),
+      bottomNavigationBar: CupertinoTabBar(
+        backgroundColor: mobileBackgroundColor,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home_outlined,
+              color: _page == 0 ? primaryColor : secondaryColor,
+            ),
+            label: "",
+            backgroundColor: primaryColor,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.search_outlined,
+              color: _page == 1 ? primaryColor : secondaryColor,
+            ),
+            label: "",
+            backgroundColor: primaryColor,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.add_circle_outline,
+              color: _page == 2 ? primaryColor : secondaryColor,
+            ),
+            label: "",
+            backgroundColor: primaryColor,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.favorite_outline,
+              color: _page == 3 ? primaryColor : secondaryColor,
+            ),
+            label: "",
+            backgroundColor: primaryColor,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person_outline,
+              color: _page == 4 ? primaryColor : secondaryColor,
+            ),
+            label: "",
+            backgroundColor: primaryColor,
+          ),
+        ],
+      ),
+    );
   }
 }
