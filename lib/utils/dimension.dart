7ccc1df261574b9df74bed74970051e2b87df8f0
Siddhanta Acharya <siddhu12980@gmail.com>
2023-12-16 01:37:05 +0530

    Post Screen

--- lib/utils/dimension.dart
+++ lib/utils/dimension.dart
@@ -1,2 +1,13 @@
 // ignore: constant_identifier_names
+import 'package:flutter/material.dart';
+import 'package:insta/screen/add_post.dart';
+
 const webscreenlayout_size = 600;
+
+const homeScreenItms = [
+  Text("Home"),
+  Text("search"),
+  addPost(),
+  Text("fav"),
+  Text("account"),
+];
